949196db7372312ebb28be64fdf145d943e53daa
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2025-04-11 21:35:01 +0500

    frontend deploy error fix

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -14,28 +14,28 @@ import {
   UserAvailabilityResponseType,
   UserEventListResponse,
   UserMeetingsResponseType,
-} from "@/types/api.type";
-import { API, PublicAPI } from "./axios-client";
-import { IntegrationAppType, VideoConferencingPlatform } from "./types";
+} from '@/types/api.type';
+import { API, PublicAPI } from './axios-client';
+import { IntegrationAppType, VideoConferencingPlatform } from './types';
 
 export const loginMutationFn = async (
-  data: loginType
+  data: loginType,
 ): Promise<LoginResponseType> => {
-  const response = await API.post("/auth/login", data);
+  const response = await API.post('/auth/login', data);
   return response.data;
 };
 
 export const registerMutationFn = async (data: registerType) =>
-  await API.post("/auth/register", data);
+  await API.post('/auth/register', data);
 
 //*********** */ EVENT APIS
 export const CreateEventMutationFn = async (data: CreateEventPayloadType) =>
-  await API.post("/event/create", data);
+  await API.post('/event/create', data);
 
 export const toggleEventVisibilityMutationFn = async (data: {
   eventId: string;
 }): Promise<ToggleEventVisibilityResponseType> => {
-  const response = await API.put("/event/toggle-privacy", data);
+  const response = await API.put('/event/toggle-privacy', data);
   return response.data;
 };
 
@@ -47,7 +47,7 @@ export const geteventListQueryFn = async (): Promise<UserEventListResponse> => {
 //*********** */ INTEGRATION APIS
 
 export const checkIntegrationQueryFn = async (
-  appType: VideoConferencingPlatform
+  appType: VideoConferencingPlatform,
 ) => {
   const response = await API.get(`integration/check/${appType}`);
   return response.data;
@@ -60,7 +60,7 @@ export const getAllIntegrationQueryFn =
   };
 
 export const connectAppIntegrationQueryFn = async (
-  appType: IntegrationAppType
+  appType: IntegrationAppType,
 ) => {
   const response = await API.get(`integration/connect/${appType}`);
   return response.data;
@@ -75,19 +75,19 @@ export const getUserAvailabilityQueryFn =
   };
 
 export const updateUserAvailabilityMutationFn = async (
-  data: AvailabilityType
+  data: AvailabilityType,
 ) => {
-  const response = await API.put("/availability/update", data);
+  const response = await API.put('/availability/update', data);
   return response.data;
 };
 
 //*********** */ Meeting APIS
 
 export const getUserMeetingsQueryFn = async (
-  filter: PeriodType
+  filter: PeriodType,
 ): Promise<UserMeetingsResponseType> => {
   const response = await API.get(
-    `/meeting/user/all${filter ? `?filter=${filter}` : ""}`
+    `/meeting/user/all${filter ? `?filter=${filter}` : ''}`,
   );
   return response.data;
 };
@@ -99,7 +99,7 @@ export const cancelMeetingMutationFn = async (meetingId: string) => {
 
 //*********** */ All EXTERNAL/PUBLIC APIS
 export const getAllPublicEventQueryFn = async (
-  username: string
+  username: string,
 ): Promise<PublicEventResponseType> => {
   const response = await PublicAPI.get(`/event/public/${username}`);
   return response.data;
@@ -110,14 +110,13 @@ export const getSinglePublicEventBySlugQueryFn = async (data: {
   slug: string;
 }): Promise<PublicSingleEventDetailResponseType> => {
   const response = await PublicAPI.get(
-    `/event/public/${data.username}/${data.slug}`
+    `/event/public/${data.username}/${data.slug}`,
   );
   return response.data;
 };
 
 export const getPublicAvailabilityByEventIdQueryFn = async (
   eventId: string,
-  timezone?: string
 ): Promise<PublicAvailabilityEventResponseType> => {
   const response = await PublicAPI.get(`/availability/public/${eventId}`);
   return response.data;
@@ -125,6 +124,6 @@ export const getPublicAvailabilityByEventIdQueryFn = async (
 
 //Create Meeting Eventid
 export const scheduleMeetingMutationFn = async (data: CreateMeetingType) => {
-  const response = await API.post("/meeting/public/create", data);
+  const response = await API.post('/meeting/public/create', data);
   return response.data;
 };
